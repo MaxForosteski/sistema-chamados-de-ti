1946d53b853b55fffa703ab3b51bd25d77666c90
MaxForosteski <maxforosteski12@gmail.com>
2023-10-23 12:55:28 -0300

    adicionado sidebar

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const fs = require("fs")
 const app = express();
 
 http.createServer((req,res)=>{
-    fs.readFile('client/src/index.html', (err,data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
+    app.get('/', (req,res)=>{
+        res.sendFile('client/src/index.html');
+    })
 }).listen(3000, () => {
     console.log('listening on port 3000');
 })
